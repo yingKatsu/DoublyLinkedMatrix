7c4b3514d0a48ce6f85358025d73498e691ea257
yingKatsu <89200882+yingKatsu@users.noreply.github.com>
2022-05-02 00:17:27 -0500

    Create TextAdventureGame.h

---
+++ TextAdventureGame.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class GameRoom {
+private:
+	std::string description;
+	std::vector<std::string> items;
+public:
+	GameRoom() {
+		//empty space, nothing here
+
+	}
+};
+
+class GameFunction {
+
+};
